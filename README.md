224e60d74a66a06dc4abd13a4fc1bb9c0de4f0ba
John Egan <john@jenca.io>
2018-03-17 13:16:34 +0000

    init

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # AEC-APIs
-A collective list of JSON APIs for web development in the AEC industries. 
+A collective list of APIs for web development in the AEC industries. 
+
+Contributing guide is coming soon.
+
+## Index
+
+* [BIM APIs](#bim-apis)
+
+### BIM APIs
+API | Description | Auth | HTTPS | CORS | Link |
+|---|---|---|---|---|---|
+| Aconex | Description | No | No | Unknown | [Go!](https://help.aconex.com/aconex/aconex-api) |
+| Acuite | Description | No | No | Unknown | [Go!](https://google.com) |
+| ArcGIS | Description | No | No | Unknown | [Go!](https://developers.arcgis.com/web-api/) |
+| Archiplan | Description | No | No | Unknown | [Go!](https://google.com) |
+| Areo | Description | No | No | Unknown | [Go!](https://google.com) |
+| Asite | Description | No | No | Unknown | [Go!](https://upload.asite.com/dmshelp/About_API.htm) |
+| Assemble | Description | No | No | Unknown | [Go!](https://google.com) |
+| Asta Powerproject | Description | No | No | Unknown | [Go!](https://google.com) |
+| Azuqua | Description | No | No | Unknown | [Go!](https://google.com) |
+| Azure Active Directory SSO | Description | No | No | Unknown | [Go!](https://google.com) |
---
+++ build/main.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+FORMAT_FILE=../README.md
+echo "running format validation..."
+./validate_format.py $FORMAT_FILE
+if [[ $? != 0 ]]; then
+    echo "format validation failed!"
+    exit 1
+fi
+echo "format validation passed!"
+./build.sh
+if [[ $? != 0 ]]; then
+    echo "JSON build failed!"
+else
+    echo "JSON build success!"
+fi
+if [ "$TRAVIS_BRANCH" == "master" ]
+then
+    echo "Master build - deploying JSON"
+    ./deploy.sh
+fi
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    echo "running on $TRAVIS_BRANCH branch - skipping Pull Request logic"
+    exit 0
+fi
+echo "running on Pull Request #$TRAVIS_PULL_REQUEST"
+DIFF_URL="https://patch-diff.githubusercontent.com/raw/bimlauncher/AEC-APIs/pull/$TRAVIS_PULL_REQUEST.diff"
+curl $DIFF_URL > diff.txt
+echo "------- BEGIN DIFF -------"
+cat diff.txt
+echo "-------- END DIFF --------"
+cat diff.txt | egrep "\+" > additions.txt
+echo "------ BEGIN ADDITIONS -----"
+cat additions.txt
+echo "------- END ADDITIONS ------"
+LINK_FILE=additions.txt
+
+echo "checking if /json was changed..."
+if egrep "\+{3}\s.\/json\/" diff.txt > json.txt; then
+    echo "JSON files are auto-generated! Please do not update these files:"
+    cat json.txt
+    exit 1
+else
+    echo "/json check passed!"
+    rm json.txt
+fi
+
+echo "running link validation..."
+./validate_links.py $LINK_FILE
+if [[ $? != 0 ]]; then
+    echo "link validation failed!"
+    exit 1
+else
+    echo "link validation passed!"
+fi
